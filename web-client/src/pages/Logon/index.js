3091da7e25aab2d994eb5bb513d8b28196313f36
viniOlimpio <viniolimpio3@gmail.com>
2020-06-29 15:02:57 -0300

    setTimeout Function added

--- web-client/src/pages/Logon/index.js
+++ web-client/src/pages/Logon/index.js
@@ -1,28 +1,36 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
-import {FiLogIn} from 'react-icons/fi'
+import { Link } from 'react-router-dom';
+import { FiLogIn } from 'react-icons/fi'
 import './styles.css';
 import logoImg from '../../assets/logo.png';
-import trabalhadorImg from '../../assets/trabalhadores.png'
+import logonImg from '../../assets/logonImg.png';
 
-export default function Logon(){
+export default function Logon() {
     return (
         <div className="logon-container">
-            <section className="form">    
-            <img className="logo" src={logoImg} alt="logo"/>
-            
-            <form>
-                <h1>Faça seu Login</h1>
-                <input placeholder="E-mail"/>
-                <input placeholder="Senha"/>
-                <button className="button" type="submit">Entrar</button>
-                <Link className="back-link" to="/register">
-                    <FiLogIn size={16} color="#14b3b0" />
+            <section className="form">
+                <nav>
+                    <a href="/home">Quem Somos</a>
+                </nav>
+                <img className="logo" src={logoImg} alt="logo" />
+                <form>
+                    <h1>Faça seu Login</h1>
+                    <input type="email" placeholder="E-mail" />
+                    <input type="password" placeholder="Senha" />
+                    
+                    <Link className="back-link" to="/home">
+                    <button className="button" type="submit">Entrar</button>
+                    </Link>
+
+                    <Link className="back-link" to="/register">
+                        <FiLogIn size={16} color="#14b3b0" />
                     Não tenho cadastro
                 </Link>
-            </form>
+                </form>
             </section>
-            <img src={trabalhadorImg} alt="trabalhador"/>
+            <div className="img">
+            <img src={logonImg} alt="trabalhador" />
+            </div>
         </div>
     );
 }
