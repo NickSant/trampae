fd03d9c0de559aa7343aead11c3694000ade718a
viniOlimpio <viniolimpio3@gmail.com>
2020-07-13 18:05:33 -0300

    update gitignore

--- mobile/node_modules/@react-navigation/stack/lib/typescript/src/utils/PreviousSceneContext.d.ts
+++
@@ -1,5 +0,0 @@
-import * as React from 'react';
-import type { Route } from '@react-navigation/native';
-import type { Scene } from '../types';
-declare const PreviousSceneContext: React.Context<Scene<Route<string>> | undefined>;
-export default PreviousSceneContext;
--- server/node_modules/joi-phone-validator/index.js
+++
@@ -1,1 +0,0 @@
-module.exports = require('./lib/phone_validator')
