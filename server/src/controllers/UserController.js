3e56c6d7e43286a4993febbc68ea89fd8b678304
Nicolas Santos <nicolasalmeidasantos@gmail.com>
2020-07-12 21:47:34 -0300

    GoogleOAuth created

--- server/src/controllers/UserController.js
+++ server/src/controllers/UserController.js
@@ -5,7 +5,7 @@ import connection from "../database/connection";
 import * as jwt from "../setup/jwt";
 
 import argon2 from "argon2"; //algoritmo de hash
-import fs from 'fs';
+import fs from "fs";
 
 import { profile } from "console";
 import Util from "../helpers/Util";
@@ -15,21 +15,19 @@ const util = new Util();
 export default {
   //list users
   async index(request, response) {
-
     const { page = 1 } = request.query;
 
-    const user = await connection('users')
-    .select("*")
-    .limit(12)
-    .offset( (page - 1) * 12);
+    const user = await connection("users")
+      .select("*")
+      .limit(12)
+      .offset((page - 1) * 12);
 
-    const res_user = user.map( item => {
+    const res_user = user.map((item) => {
       //deletando senha do objeto de retornos
       delete item.password;
       return item;
     });
-    
-    
+
     return response.json(res_user);
   },
   //create user
@@ -42,9 +40,9 @@ export default {
 
     const id = await crypto.randomBytes(4).toString("HEX");
 
-    const token = await jwt.generateToken({ user_id: id });//gerando token para auth
+    const token = await jwt.generateToken({ user_id: id }); //gerando token para auth
 
-    try{
+    try {
       await connection("users").insert({
         id,
         name,
@@ -56,31 +54,33 @@ export default {
       });
 
       console.log(data);
-    }catch(e){
+    } catch (e) {
       console.log(e.sqlMessage);
-      if(e.sqlMessage.includes('users_email_unique')){
-
+      if (e.sqlMessage.includes("users_email_unique")) {
         response.status(406);
-        return response.json({err:'Duplicated email'})
-
-      }else if(e.sqlMessage.includes('users_whatsapp_unique')){
-
+        return response.json({ err: "Duplicated email" });
+      } else if (e.sqlMessage.includes("users_whatsapp_unique")) {
         response.status(406);
-        return response.json({err:'Duplicated Whatsapp'});
-
+        return response.json({ err: "Duplicated Whatsapp" });
       }
-      console.log(e)
-      return response.json({Error:`Database Error: ${e}`})
+      console.log(e);
+      return response.json({ Error: `Database Error: ${e}` });
     }
-      
+
     return response.json({ id, token });
+  },
+  async GoogleOAuth(req, res) {
+    const user_id = req.user[0].id;
+    const token = await jwt.generateToken({ user_id });
+
+    res.status(200).json({ token });
   },
 
   async login(req, res) {
     const [hashTyp, hash] = req.headers.authorization.split(" "); //Basic Authenticate. Formato: Basic HASH
     const [email, password] = Buffer.from(hash, "base64").toString().split(":"); //Buffer - descriptografa um hash -> separado por :
     //Tudo isso vindo dos headers! Pra não deixar exposto (plain-text) no header, os dados que o usuário envia
-    console.log('inicio de login');
+    console.log("inicio de login");
     try {
       if (
         !email.includes("@") ||
@@ -93,29 +93,28 @@ export default {
         res.status(401, { error: "Malformated Elements" });
         return res.json({ Error: "Malformated Elements" });
       }
-      console.log('passou validação')
-      
+      console.log("passou validação");
+
       const result = await connection("users")
-      .select("*")
-      .where("email", email)
-      .first();
+        .select("*")
+        .where("email", email)
+        .first();
 
-      console.log(result)
+      console.log(result);
 
-      if(!result || result === undefined){
-        return res.status(400).json({Error:'User not found'})
+      if (!result || result === undefined) {
+        return res.status(400).json({ Error: "User not found" });
       }
       const pass_bd = await Buffer.from(result.password, "base64").toString(); //DECODIFICANDO HASH DO PRÓPRIO MYSQL!!! - também é do tipo buffer!
 
-      console.log('decodificou buffer');
+      console.log("decodificou buffer");
 
       //argon2.verify (HASHED_PASS, plainTextPassword)
-      if (!(await argon2.verify(pass_bd, password))){
+      if (!(await argon2.verify(pass_bd, password))) {
         console.log("senhas diferentes");
         res.status(400);
-        return res.json({Error:'Senhas Incorreta'});
+        return res.json({ Error: "Senhas Incorreta" });
       }
-        
 
       if (email !== result.email || !(await argon2.verify(pass_bd, password))) {
         res.status(401, { error: "Incorrect username or password" });
@@ -138,77 +137,65 @@ export default {
       };
 
       res.json({ user: user, token: token });
-
     } catch (err) {
-      console.log('Deu pau..');
+      console.log("Deu pau..");
       res.status(401, { error: err });
-
     }
   },
 
-  async profile(req, res){
-    
+  async profile(req, res) {
     const { id } = req.params;
-    
-    const { id:req_id } = req.auth;//id do user autenticado e logado
-    
-    const exists = await connection('users').select('*')
-    .where('id',id)
-    .first();
 
-    if(!exists || exists === undefined || exists === '') {
+    const { id: req_id } = req.auth; //id do user autenticado e logado
+
+    const exists = await connection("users")
+      .select("*")
+      .where("id", id)
+      .first();
+
+    if (!exists || exists === undefined || exists === "") {
       console.log(`User ${id} not exists`);
       res.status(401, { error: "User not Found" });
       return res.json({ Error: "User not Found" });
     }
     delete exists.password;
-      
 
-    if(id === req_id){
+    if (id === req_id) {
       //ĺógica para a possibilidade de editar os dados!!
-      console.log('USER ENTROU NO PRÓPRIO PERFIL');
-
-
-    }else{
+      console.log("USER ENTROU NO PRÓPRIO PERFIL");
+    } else {
       //dados são somente visíveis ao user 'requisitante'
-      console.log('USER ENTROU NO PERFIL DE OUTRO');
-
+      console.log("USER ENTROU NO PERFIL DE OUTRO");
     }
-    
-    
+
     return res.json(exists);
-    
-
-
   },
-  async uploadImage(req, res){
-    console.log('uploadController')
-    try{      
-      const [,tipoImg] = req.file.mimetype.split('/');
-      
-      const { name:original_name, id } = req.auth;
-      if(!id || id === undefined || id === '')  
-        return res.json({Error:'User not valid'})
-      
-      if(req.file === undefined || !req.file)
-        return res.json({Error:'Archive not exists'});
-      
+  async uploadImage(req, res) {
+    console.log("uploadController");
+    try {
+      const [, tipoImg] = req.file.mimetype.split("/");
+
+      const { name: original_name, id } = req.auth;
+      if (!id || id === undefined || id === "")
+        return res.json({ Error: "User not valid" });
+
+      if (req.file === undefined || !req.file)
+        return res.json({ Error: "Archive not exists" });
 
       const name_user = util.clearString(original_name);
       console.log(name_user);
-      
 
-
-
-      fs.rename(`./uploads/${req.file.originalname}`,//nome antigo
+      fs.rename(
+        `./uploads/${req.file.originalname}`, //nome antigo
         `./uploads/${name_user}-${id}.${tipoImg}`, //novo nome
-        (err) =>{//catch
-          if(err){
-              console.error(err);
-              res.status(400);
-              return res.json({Error:'Erro'});
+        (err) => {
+          //catch
+          if (err) {
+            console.error(err);
+            res.status(400);
+            return res.json({ Error: "Erro" });
           }
-          console.log('Arquivo renomeado')
+          console.log("Arquivo renomeado");
         }
       );
       //NOME_USER-ID_USER.TIPO
@@ -216,22 +203,19 @@ export default {
       req.file.filename = `${name_user}-${id}.${tipoImg}`;
       //caminho da imagem
       req.file.path = `uploads/${req.file.filename}`;
-      
-      await connection('users').update({
-        image_url: req.file.path
-      }).where('id',id);
-      console.log('inseriu path image no banco');
 
-      return res.json({image: req.file});
+      await connection("users")
+        .update({
+          image_url: req.file.path,
+        })
+        .where("id", id);
+      console.log("inseriu path image no banco");
 
-    }catch(e){
+      return res.json({ image: req.file });
+    } catch (e) {
       console.log(e);
       res.status(400);
-      return res.json({Error:e})
+      return res.json({ Error: e });
     }
-
   },
-
-
-  
 };
--- server/src/routes.js
+++ server/src/routes.js
@@ -1,24 +1,35 @@
 import express from "express";
+import passport from "passport";
 
 import UserController from "./controllers/UserController";
 import ServiceController from "./controllers/ServiceController";
 
-import { validateBody, schemas} from "./helpers/validation"
-import multer from './multer';
+import { validateBody, schemas } from "./helpers/validation";
+import multer from "./multer";
 
 import authMiddleware from "./setup/auth";
 import SearchController from "./controllers/SearchController";
 
-
+import passportConf from "./passport";
 
 const routes = express.Router();
 
 //SignUp rota
-routes.post("/user",validateBody(schemas.signUpSchema), UserController.create);
+routes.post(
+  "/signup",
+  validateBody(schemas.signUpSchema),
+  UserController.create
+);
+
+//GoogleOAuth
+routes.post(
+  "/oauth/google",
+  passport.authenticate("googleToken", { session: false }),
+  UserController.GoogleOAuth
+);
 
 //SignIn rota
-routes.get("/login", UserController.login);
-
+routes.post("/login", UserController.login);
 
 //listar usuários - development
 routes.get("/user", UserController.index);
@@ -31,7 +42,6 @@ routes.use(authMiddleware); //authenticator
 //pra todas as próximas rotas, o servidor vai passar por esse middleware pra verificar se o token do usuário, passado pelo Bearer da requisição é válido
 // logo, em todas preciso passar no header da req, um authorization do tipo Bearer!!!!
 
-
 routes.get("/me", (req, res) => {
   //rota para usar no client, que busca qual usuário foi autenticado. (ver arquivo auth.js)
   //de acordo com o bearer token
@@ -43,15 +53,23 @@ routes.get("/me", (req, res) => {
 //as imagens devem ser por um form no insomnia!!
 //as configurações da imagens podem ser acessadas no controller com esse nome "img_perfil"
 //o name do campo que enviar a imagem, deve ser exatamente igual ao do .single()
-routes.post('/upload-image', multer.single('img_perfil'), UserController.uploadImage);
+routes.post(
+  "/upload-image",
+  multer.single("img_perfil"),
+  UserController.uploadImage
+);
 
-routes.get('/user/:id', UserController.profile);
+routes.get("/user/:id", UserController.profile);
 
 //searches
 routes.get("/search/:uf/:city/:cat_id", SearchController.SearchServices);
 routes.get("/search/:name", SearchController.SearchUsers);
 
-routes.post("/services", validateBody(schemas.serviceSchema), ServiceController.create);
+routes.post(
+  "/services",
+  validateBody(schemas.serviceSchema),
+  ServiceController.create
+);
 routes.delete("/services/:id", ServiceController.delete);
 
 export default routes;
