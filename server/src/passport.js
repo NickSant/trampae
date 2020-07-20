b06b6bcff1e09548c9126a6941141744ac8cf0ec
Nicolas Santos <nicolasalmeidasantos@gmail.com>
2020-07-20 13:10:07 -0300

    Facebook OAuth added

--- server/src/passport.js
+++ server/src/passport.js
@@ -2,6 +2,7 @@ import passport from "passport";
 import crypto from "crypto";
 
 const GooglePlusTokenStrategy = require("passport-google-plus-token");
+const FacebookStrategy = require("passport-facebook-token");
 
 require("dotenv/config");
 
@@ -18,7 +19,47 @@ passport.use(
       try {
         const existingUser = await connection("users")
           .select("*")
-          .where("email", profile.emails[0].value);
+          .where("third_party_id", profile.id);
+
+        if (existingUser.length === 1) {
+          done(null, existingUser);
+          return;
+        }
+
+        const id = crypto.randomBytes(4).toString("HEX");
+
+        await connection("users").insert({
+          id,
+          third_party_id: profile.id,
+          name: profile.displayName,
+          email: profile.emails[0].value,
+          image_url: profile.photos[0].value,
+        });
+
+        const user = await connection("users").select("*").where("id", id);
+
+        done(null, user);
+      } catch (error) {
+        done(error, false);
+      }
+    }
+  )
+);
+
+passport.use(
+  "facebookToken",
+  new FacebookStrategy(
+    {
+      clientID: process.env.FB_CLIENT_ID,
+      clientSecret: process.env.FB_CLIENT_SECRET
+    },
+    async (accessToken, refreshToken, profile, done) => {
+      try {
+        console.log(accessToken);
+        console.log(profile);
+        const existingUser = await connection("users")
+          .select("*")
+          .where("third_party_id", profile.id);
 
         if (existingUser.length === 1) {
           done(null, existingUser);
--- server/src/routes.js
+++ server/src/routes.js
@@ -4,8 +4,8 @@ import passport from "passport";
 import UserController from "./controllers/UserController";
 import ServiceController from "./controllers/ServiceController";
 
-import { validateBody, schemas} from "./helpers/validation"
-import multer from './multer';
+import { validateBody, schemas } from "./helpers/validation";
+import multer from "./multer";
 
 import authMiddleware from "./setup/auth";
 import SearchController from "./controllers/SearchController";
@@ -16,7 +16,7 @@ const routes = express.Router();
 
 //SignUp rota
 routes.post(
-  '/signup',
+  "/signup",
   validateBody(schemas.signUpSchema),
   UserController.create
 );
@@ -24,14 +24,18 @@ routes.post(
 //SignIn rota
 routes.post("/login", UserController.login);
 
-
 //GoogleOAuth
 routes.post(
   "/oauth/google",
   passport.authenticate("googleToken", { session: false }),
-  UserController.GoogleOAuth
+  UserController.OAuth
 );
 
+routes.post(
+  "/oauth/facebook",
+  passport.authenticate("facebookToken", { session: false }),
+  UserController.OAuth
+);
 
 //listar usuários - development
 routes.get("/user", UserController.index);
@@ -44,23 +48,26 @@ routes.use(authMiddleware); //authenticator
 //pra todas as próximas rotas, o servidor vai passar por esse middleware pra verificar se o token do usuário, passado pelo Bearer da requisição é válido
 // logo, em todas preciso passar no header da req, um authorization do tipo Bearer!!!!
 
-
 routes.get("/me", (req, res) => {
   //rota para usar no client, que busca qual usuário foi autenticado. (ver arquivo auth.js)
   //de acordo com o bearer token
   res.send(req.auth); //esse parâmetro é setado em auth.js
 });
 
-routes.put('/update/:type', UserController.updateData);
+routes.put("/update/:type", UserController.updateData);
 
 //UPLOAD DE IMAGENS - Perfil
 //setando middleware multer.js
 //as imagens devem ser por um form no insomnia!!
 //as configurações da imagens podem ser acessadas no controller com esse nome "img_perfil"
 //o name do campo que enviar a imagem, deve ser exatamente igual ao do .single()
-routes.post('/upload-image', multer.single('img_perfil'), UserController.uploadImage);
+routes.post(
+  "/upload-image",
+  multer.single("img_perfil"),
+  UserController.uploadImage
+);
 
-routes.get('/user/:id', UserController.profile);
+routes.get("/user/:id", UserController.profile);
 
 //searches
 routes.get("/search/:uf/:city/:cat_id", SearchController.SearchServices);
