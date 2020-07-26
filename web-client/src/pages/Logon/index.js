26ff795c812291215bb11d8f0d7358aebe8d512e
Nicolas Santos <nicolasalmeidasantos@gmail.com>
2020-07-26 00:52:01 -0300

    login page design refactored

--- web-client/src/pages/Logon/index.js
+++ web-client/src/pages/Logon/index.js
@@ -1,52 +1,31 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import { FiLogIn, FiSun } from 'react-icons/fi'
-import './styles.css';
-import logoImg from '../../assets/logo.png';
-import logonImg from '../../assets/logonImg.png';
+import React from "react";
+import { Link } from "react-router-dom";
+import { FiLogIn, FiSun } from "react-icons/fi";
+import "./styles.css";
+import logoImg from "../../assets/logo.png";
+import logonImg from "../../assets/logonImg.png";
+
+import Form from "../../components/loginForm/loginForm";
 
 export default function Logon() {
-    return (
-        <div className="logon-container">
-            <section className="form">
-                {/*NavBar e header*/}
-                <nav>
-                    <header>
-                        <button className="aboutUs-button">
-                            <Link id="aboutUs" className="rota-link" to="/aboutus">
-                                <p>
-                                    Quem Somos?
-                                </p>
-                            </Link>
-                        </button>
-                        <button className="theme-button">
-                            <FiSun size={20} />
-                        </button>
-                    </header>
-
-                {/*Meio do site*/}
-                </nav>
-                <img className="logo" src={logoImg} alt="logo" />
-                <form>
-                    <h1>Faça seu Login</h1>
-                    <input type="email" placeholder="E-mail" />
-                    <input type="password" placeholder="Senha" />
-
-                    <Link className="back-link" to="/home">
-                        <button className="button" type="submit">Entrar</button>
-                    </Link>
-
-                    <Link className="back-link" to="/register">
-                        <FiLogIn size={16} color="#14b3b0" />
-                    Não tenho cadastro
-                </Link>
-                </form>
-            </section>
-
-            {/*trabalhadores*/}
-            <div className="img">
-                <img src={logonImg} alt="trabalhador" />
-            </div>
+  return (
+    <div className="container">
+      <div className="box">
+        <div className="login">
+          <div className="login-header">
+            <img src={logoImg} alt="Trampaê"></img>
+          </div>
+          <div className="form-container">
+            <h1 className="title"> Faça seu login! </h1>
+            <Form />
+          </div>
         </div>
-    );
+        <div className="disabled-register">
+          <h1 className="title"> Ainda não tem Login? </h1>
+          <h3 className="title"> tá esperando o que?</h3>
+          <button className="button"> Registre-se já</button>
+        </div>
+      </div>
+    </div>
+  );
 }
