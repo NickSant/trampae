814734e8df389fa6787e045c34506b4d9ce0bc67
viniOlimpio <viniolimpio3@gmail.com>
2020-08-12 19:08:38 -0300

    login integration mobile

--- mobile/src/pages/Home/index.js
+++ mobile/src/pages/Home/index.js
@@ -1,8 +1,8 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 
 import estilo from './styles';
-import { View, Text, Image, Button, TouchableOpacity} from 'react-native';
+import { View, Text, Image, Button, TouchableOpacity, AsyncStorage, ToastAndroid} from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import logoImg from '../../assets/icon.png';
 import { TextInput, ScrollView } from 'react-native-gesture-handler';
@@ -13,6 +13,33 @@ import api from '../../services/api';
 export default function Index(){
 
     const nav = useNavigation();
+
+    const [user, setUser] = useState({});
+
+
+    useEffect(() =>{
+        const token = AsyncStorage.getItem('token')
+        .then(token =>{
+            if(!token){
+                ToastAndroid.show('Você não é autorizado aqui seu bosta.', ToastAndroid.LONG);
+                nav.navigate('login');
+            }
+            ToastAndroid.show(`TOKEN: ${token}`, ToastAndroid.LONG);
+
+            fetch(`${api}/me`,{
+                method:'GET', 
+                headers:{
+                    "Content-Type": "application/json",
+                    "authorization":`Bearer ${token}`
+                }
+            }).then( res => res.json().then( data =>{
+                console.log(`USER:${data.name}`)
+                setUser(data);
+            }));
+        })
+        
+    },[])
+
 
     function goToRegister(){
         nav.navigate('register')
@@ -32,7 +59,7 @@ export default function Index(){
         <View style={estilo.container}>
             
             <Text>
-                HOMEEEE 
+                Bem vindo, {user.name}
             </Text>
         </View>
     );
--- mobile/src/pages/Login/index.js
+++ mobile/src/pages/Login/index.js
@@ -1,13 +1,17 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 
+
 import estilo from './styles';
-import { View, Text, Image, Button, TouchableOpacity} from 'react-native';
+import { View, Text, Image, Button, TouchableOpacity, ToastAndroid, AsyncStorage} from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import logoImg from '../../assets/icon.png';
 import { TextInput, ScrollView } from 'react-native-gesture-handler';
 
+
 import api from '../../services/api';
+import baes64 from 'react-native-base64';
+import base64 from 'react-native-base64';
 
 
 export default function Index(){
@@ -18,12 +22,46 @@ export default function Index(){
         nav.navigate('register')
     }
 
+    const [mail, setMail] = useState('');
+    const [pass, setPass] = useState('');
 
-    
+    function submit(){
+        AsyncStorage.clear();
+        const hash = `Basic ${base64.encode(`${mail}:${pass}`)}`;
+
+        fetch(`${api}/login`,{
+            method:'POST',
+            headers:{
+                "Content-Type": "application/json",
+                'authorization': hash,
+            },
+        }).then(res =>{
+            res.json().then(data =>{
+                console.log(data);
+                if(data.Error)
+                    return ToastAndroid.show('Usuário ou senha incorretos', ToastAndroid.LONG);
+
+                const user = data.user;
+                const token = data.token;
+
+                if(!user || !token)
+                    return ToastAndroid.show('Ocorreu algum erro..\nTente novamente mais tarde', ToastAndroid.LONG);
+
+                AsyncStorage.setItem('token', token);
+
+                ToastAndroid.show(`Bem vindo novamente ${user.name}`, ToastAndroid.LONG);
+                nav.navigate('home');
+                
+            })
+            .catch(e =>{
+                console.log(e);
+            })
+        })
+    }
+
 
     useEffect(() =>{
         console.log('iniciou');
-        
     },[])
 
     
@@ -40,7 +78,7 @@ export default function Index(){
                     <View style={estilo.input_group}>
                         
                         <TextInput 
-                            
+                            onChange={e => setMail(e.nativeEvent.text)}
                             keyboardType="email-address" 
                             style={estilo.input}
                             autoCapitalize="none"
@@ -52,6 +90,7 @@ export default function Index(){
                     </View>
                     <View style={estilo.input_group}>
                         <TextInput 
+                            onChange={e => setPass(e.nativeEvent.text)}
                             keyboardType="visible-password" 
                             multiline={false}  
                             style={estilo.input}
@@ -62,7 +101,7 @@ export default function Index(){
                         />
                     </View>
                     <TouchableOpacity style={estilo.button}>
-                        <Text style={estilo.p_button}>Entrar</Text>
+                        <Text onPress={submit} style={estilo.p_button}>Entrar</Text>
                     </TouchableOpacity>
                     <TouchableOpacity onPress={goToRegister} style={estilo.link_cadastro}>
                         <Text style={estilo.p_link}>Não possui um cadastro?</Text>
