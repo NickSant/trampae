440199e43dd3369408f844de37730cbd6564671f
viniOlimpio <viniolimpio3@gmail.com>
2020-10-14 18:30:39 -0300

    mobile styled components

--- mobile/src/routes.js
+++ mobile/src/routes.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+import {FontAwesome} from '@expo/vector-icons'
 
 import Login from './pages/Login/index';
 import Register from './pages/Register/index';
@@ -8,15 +11,39 @@ import Home from './pages/Home/index';
 
 //Navegação do tipo stack != da navegação por menu lateral ou menu "em baixo"
 const AppStack = createStackNavigator();
+const Tab = createBottomTabNavigator();
 export const Routes = () =>{
 
     return(
-        <NavigationContainer>
-            <AppStack.Navigator screenOptions={{headerShown:false}}>
-                <AppStack.Screen name="login" component={Login} />
-                <AppStack.Screen name="register" component={Register} />
-                <AppStack.Screen name="home" component={Home} />
-            </AppStack.Navigator>
-        </NavigationContainer>
+        <>
+            <NavigationContainer>
+                <AppStack.Navigator screenOptions={{headerShown:false}}>
+                    <AppStack.Screen name="login" component={Login} />
+                    <AppStack.Screen name="register" component={Register} />
+                    <AppStack.Screen name="home" component={Home} />
+                </AppStack.Navigator>
+            </NavigationContainer>
+            {/* <NavigationContainer>
+                <Tab.Navigator
+                    screenOptions={({route}) =>({
+                        tabBarIcon: ({focused, color, size}) =>{
+                            let iconName;
+                            if(route.name === 'home') iconName = 'fa fa-home'
+                            else if(route.name === 'perfil') iconName = 'fa fa-user'
+                            //COLOAR ICONES PRA OUTRAS ROTAS!!!
+                            return <FontAwesome name={iconName} size={size} color={color} />
+                        }
+                    })}
+                    tabBarOptions={{
+                        activeTintColor: 'blue',
+                        inactiveTintColor: 'red',
+
+                    }}
+                >
+                    <Tab.Screen name="home" component={Home} />
+                    <Tab.Screen name="login" component={Login} />
+                </Tab.Navigator>
+            </NavigationContainer> */}
+        </>
     );
 }
