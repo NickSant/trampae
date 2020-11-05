e4bfeb3e76735c1792e6f707c39cc229372c1fdb
Nicolas Santos <nicolasalmeidasantos@gmail.com>
2020-11-04 22:45:19 -0300

    fixed navbar styles

--- web-client/src/components/Navbar/index.js
+++ web-client/src/components/Navbar/index.js
@@ -1,65 +1,21 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import { NavBar, LogoTrampae, IconSearch, ContainerLeft, ContainerMiddle, ContainerRight, Profile, IconHome, IconUser, IconHelp, IconPhone, IconPlus, IconGetOut } from './styles'
+import { Container } from './styles'
 
-import IconTrampae from '../../assets/icon.png'
+import trampaeIcon from '../../assets/icon.png'
 import ProfileImg from '../../assets/profile.png'
-import './styles.js'
 
-//---Começo do Front-end---//
+
 export default function Navbar() {
 	return (
-		<NavBar>
-			{/* Container da esquerda */}
-			<ContainerLeft>
-				<LogoTrampae>
-					<a href="/home">
-						<img src={IconTrampae} alt="logo" />
-					</a>
-				</LogoTrampae>
+		<Container>
+			<img src={trampaeIcon} alt="Logo"/>
 
-				<button>
-					<IconSearch />
-					<p>Buscar Bicos Na sua região</p>
+			<Link to="/create-post">
+				<button className="button">
+					Novo bico
 				</button>
-			</ContainerLeft>
-
-			{/* Container do meio */}
-			<ContainerMiddle>
-				<Link to="/home">
-					<IconHome />
-				</Link>
-
-				<Link to="/profile">
-					<IconUser />
-				</Link>
-
-				<Link to="/aboutus">
-					<IconHelp />
-				</Link>
-
-				<Link to="/talkwithus">
-					<IconPhone />
-				</Link>
-
-				<Link to="/">
-					<IconGetOut />
-				</Link>
-			</ContainerMiddle>
-
-			{/* Container da direita */}
-			<ContainerRight>
-				<Link to="/new-service" className="buttonNewService">
-					<IconPlus />
-					<p>Adicionar novo bico</p>
-				</Link>
-
-				<Profile>
-					<a href="/profile">
-						<img src={ProfileImg} alt="profile" />
-					</a>
-				</Profile>
-			</ContainerRight>
-		</NavBar>
+			</Link>
+		</Container>
 	)
 }
