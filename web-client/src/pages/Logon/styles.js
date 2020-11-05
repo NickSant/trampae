e5f0ddd208b0530568b7db60b4a36b01af996c2d
Nicolas Santos <nicolasalmeidasantos@gmail.com>
2020-11-04 22:46:13 -0300

    fixed logon styles

--- web-client/src/pages/Logon/styles.js
+++ web-client/src/pages/Logon/styles.js
@@ -76,7 +76,7 @@ export const FormContainer = styled.div`
 `;
 
 export const Title = styled.h1`
-  font-size: 50px;
+  font-size: 3.5rem;
   padding: 10px;
   border-bottom: 5px solid var(--primary);
 
