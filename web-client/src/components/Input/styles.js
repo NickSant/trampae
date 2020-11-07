b8e0ad79035010caad6b9228ff167f8f54efdee5
Nicolas Santos <nicolasalmeidasantos@gmail.com>
2020-11-06 21:05:11 -0300

    fixed input styles

--- web-client/src/components/Input/styles.js
+++ web-client/src/components/Input/styles.js
@@ -1,14 +1,5 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
-    margin-top: -1px;
-    border: none;
-    display: flex;
-    flex-direction: column;
-    justify-content: center; 
 
-    @media(max-width: 840px) {
-        height: 5rem;
-        /* width: 90vw; */
-      }
 `;
