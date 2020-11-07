4d625c64bff13d850034476517b2633116c7cd1e
Nicolas Santos <nicolasalmeidasantos@gmail.com>
2020-11-06 21:05:24 -0300

    fixed navbar backlink

--- web-client/src/components/Navbar/index.js
+++ web-client/src/components/Navbar/index.js
@@ -5,16 +5,15 @@ import { Container } from './styles'
 import trampaeIcon from '../../assets/icon.png'
 import ProfileImg from '../../assets/profile.png'
 
-
 export default function Navbar() {
 	return (
 		<Container>
-			<img src={trampaeIcon} alt="Logo"/>
+			<Link to="/home">
+				<img src={trampaeIcon} alt="Logo" />
+			</Link>
 
-			<Link to="/create-post">
-				<button className="button">
-					Novo bico
-				</button>
+			<Link to="/new-service">
+				<button className="button">Novo bico</button>
 			</Link>
 		</Container>
 	)
