a22f183233b7aae88e22e58e87b8f8c82c1f9b47
Nicolas Santos <nicolasalmeidasantos@gmail.com>
2020-11-10 21:23:12 -0300

    created DevCard component

---
+++ web-client/src/components/DevCard/index.js
@@ -0,0 +1,37 @@
+import React from 'react'
+
+import { Container } from './styles'
+
+import { RiInstagramFill, RiLinkedinBoxFill, RiGithubFill } from 'react-icons/ri'
+import { FiArrowDown, FiArrowUp } from 'react-icons/fi'
+
+function DevCard({ name, location, photoUrl, linkInstagram, linkLinkedin, linkGithub }) {
+	return (
+		<Container>
+			<img src={photoUrl} alt="dev" />
+
+			<div className="infos">
+				<div className="arrow">
+					<FiArrowUp />
+				</div>
+				<div className="bottom">
+					<strong>{name}</strong>
+					<span>{location}</span>
+				</div>
+				<div className="links">
+					<a href={linkInstagram} target="_blank">
+						<RiInstagramFill size={'1.8rem'} />
+					</a>
+					<a href={linkLinkedin} target="_blank">
+						<RiLinkedinBoxFill size={'1.8rem'} />
+					</a>
+					<a href={linkGithub} target="_blank">
+						<RiGithubFill size={'1.8rem'} />
+					</a>
+				</div>
+			</div>
+		</Container>
+	)
+}
+
+export default DevCard
