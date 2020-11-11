a2172e9e73c58ab6bd20138ba5cf3097341a9ddb
Nicolas Santos <nicolasalmeidasantos@gmail.com>
2020-11-10 21:23:39 -0300

    fixed select component styles

--- web-client/src/components/Select/styles.js
+++ web-client/src/components/Select/styles.js
@@ -1,5 +1,7 @@
 import styled from 'styled-components';
 
-export const Container = styled.div`
+export const SelectInput = styled.select`
     width: 100%;
+    font-size: 1.1rem;
+    height: 2.5rem;
 `;
