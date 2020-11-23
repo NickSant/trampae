8af08b1ecda55df5d38d2ea9ab90d4da3eefa15e
Nicolas Santos <nicolasalmeidasantos@gmail.com>
2020-11-23 17:06:25 -0300

    homepage logic added

--- web-client/src/pages/Home/index.js
+++ web-client/src/pages/Home/index.js
@@ -1,18 +1,110 @@
-import React, { useState } from 'react'
-import { Link } from 'react-router-dom'
+import React, { useEffect, useState } from "react"
+import { Link } from "react-router-dom"
 
-import Post from '../../components/Post'
-import Select from '../../components/Select'
-import Navbar from '../../components/Navbar'
+import { useAuth } from "../../contexts/authContext"
+import api from "../../services/api"
+import ibge from "../../services/ibge"
 
-import { Container, SideBar, MainContent, FilterContainer } from './styles.js'
+import Post from "../../components/Post"
+import Select from "../../components/Select"
+import Navbar from "../../components/Navbar"
 
-import { FiUsers, FiSettings, FiPhoneCall, FiLogOut, FiTrash, FiFilter, FiPlusCircle } from 'react-icons/fi'
-import profilePic from '../../assets/ZecaUrubu.png'
+import { Container, SideBar, MainContent, FilterContainer } from "./styles.js"
+
+import {
+	FiUsers,
+	FiSettings,
+	FiPhoneCall,
+	FiLogOut,
+	FiTrash,
+	FiFilter,
+	FiPlusCircle,
+} from "react-icons/fi"
+import profilePic from "../../assets/ZecaUrubu.png"
 
 //--Começo do Front-end---//
 export default function Home() {
+	const { user } = useAuth()
 	const [isFilterActive, setFilterActive] = useState(false)
+	const [services, setServices] = useState([])
+
+	const [ufs, setUfs] = useState([])
+	const [cities, setCities] = useState([])
+
+	const [selectedUf, setSelectedUf] = useState()
+	const [selectedCity, setSelectedCity] = useState()
+
+	function clearFilters() {
+		getServicesData()
+		setSelectedUf("")
+		setSelectedCity("")
+	}
+
+	async function concatData(service) {
+		const userData = await getUserData(service.user_id)
+		return { ...service, userData }
+	}
+
+	async function getUserData(userId) {
+		const apiResponse = await api.get("/search/users", {
+			params: {
+				id: userId,
+			},
+		})
+		return apiResponse.data.users[0]
+	}
+
+	async function getServicesData() {
+		const apiResponse = await api.get("services")
+		const data = apiResponse.data
+
+		const concatedData = await Promise.all(
+			data.map(service => {
+				return concatData(service)
+			})
+		)
+		setServices(concatedData)
+	}
+
+	useEffect(() => {
+		getServicesData()
+
+		async function getUfs() {
+			const ufs = await ibge.getUfs()
+
+			setUfs(ufs)
+		}
+		getUfs()
+	}, [])
+
+	useEffect(() => {
+		async function getCities() {
+			const cities = await ibge.getCities(selectedUf)
+
+			setCities(cities)
+		}
+		getCities()
+	}, [selectedUf])
+
+	async function handleSubmit(e) {
+		e.preventDefault()
+
+		const response = await api.get("/search/services", {
+			params: {
+				uf: selectedUf,
+				city: selectedCity,
+			},
+		})
+
+		const data = response.data
+		const concatedData = await Promise.all(
+			data.services.map(service => {
+				return concatData(service)
+			})
+		)
+		setServices(concatedData)
+		console.log(services)
+	}
 
 	return (
 		<>
@@ -30,51 +122,76 @@ export default function Home() {
 				<SideBar>
 					<Link to="/profile" className="navItem">
 						<img src={profilePic} alt="user" className="profilePic" />
-						Flavinho do Pneu
+						{user.name}
 					</Link>
 					<Link to="/talkwithus" className="navItem">
-						<FiPhoneCall size={'1.8rem'} />
+						<FiPhoneCall size={"1.8rem"} />
 						Fale conosco
 					</Link>
 					<Link to="/aboutus" className="navItem">
-						<FiUsers size={'1.8rem'} />
+						<FiUsers size={"1.8rem"} />
 						Sobre nós
 					</Link>
 					<Link className="navItem">
-						<FiSettings size={'1.8rem'} />
+						<FiSettings size={"1.8rem"} />
 						Configurações
 					</Link>
 
 					<Link className="navItem" to="/logout">
-						<FiLogOut size={'1.8rem'} />
+						<FiLogOut size={"1.8rem"} />
 						Sair
 					</Link>
 				</SideBar>
 
 				<MainContent filterActive={isFilterActive}>
-					<Post />
-					<Post />
-					<Post />
-					<Post />
-					<Post />
-					<Post />
-					<Post />
+					{services.map(service => {
+						return (
+							<Post
+								key={service.id}
+								id={service.id}
+								user={service.userData}
+								title={service.title}
+								price={service.price}
+								description={service.description}
+							/>
+						)
+					})}
 				</MainContent>
 			</Container>
 
 			<FilterContainer filterActive={isFilterActive}>
 				<header>
 					<strong>Filtrar bicos</strong>
-					<FiTrash />
+
+					<FiTrash onClick={clearFilters} size={"1.2rem"} />
 				</header>
 
 				<label>Estado: </label>
-				<Select />
+				<Select
+					onChange={e => setSelectedUf(e.target.value)}
+					name="UF"
+					children={ufs.map(uf => (
+						<option key={uf.id} value={uf.value} title={uf.title}>
+							{uf.value}
+						</option>
+					))}
+				/>
 
 				<label>Cidade: </label>
-				<Select />
+				<Select
+					className="select"
+					onChange={e => setSelectedCity(e.target.value)}
+					name="cidade"
+					children={cities.map(city => (
+						<option key={city.id} value={city.name} title={city.name}>
+							{city.name}
+						</option>
+					))}
+				/>
 
-				<button className="button">Filtrar</button>
+				<button onClick={handleSubmit} className="button">
+					Filtrar
+				</button>
 			</FilterContainer>
 		</>
 	)
